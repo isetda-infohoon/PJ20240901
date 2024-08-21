5cebbbd5da1810b02974abb54c8a1fe83c75846f
JungDa02 <jung.dahyeon@iset-da.com>
2024-08-21 13:35:38 +0900

    config.xml 수정, ConfigLoader 추가,GoogleService 추가, IMGFileClassifyService CNRK 모듈 수정

--- src/main/java/com/isetda/idpengine/HelloController.java
+++ src/main/java/com/isetda/idpengine/HelloController.java
@@ -3,12 +3,21 @@ package com.isetda.idpengine;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.util.List;
+
 public class HelloController {
+    private ConfigLoader configLoader = ConfigLoader.getInstance();
+    List<String> BUCKET_NAMES;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application@!");
+        BUCKET_NAMES = configLoader.getBucketNames();
+        System.out.println(configLoader.getKeyFilePath());
+        System.out.println(configLoader.getProjectId());
+        System.out.println(configLoader.getBucketNames());
+        System.out.println(BUCKET_NAMES);
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,23 @@ module com.isetda.idpengine {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
+    requires org.json;
+    requires google.cloud.storage;
+    requires com.google.auth;
+    requires com.google.auth.oauth2;
+    requires google.cloud.core;
+    requires google.cloud.vision;
+    requires com.google.common;
+    requires okhttp3;
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
+    requires com.google.gson;
+    requires jdk.compiler;
+
+    requires java.xml;
+    requires org.apache.logging.log4j;
+
+    requires java.desktop;
 
     opens com.isetda.idpengine to javafx.fxml;
     exports com.isetda.idpengine;
